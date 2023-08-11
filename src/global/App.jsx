f8274461e082cba4ae3497755d16b2f268bc0323
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-08-11 18:29:24 -0500

    Include Gameboard component

--- src/global/App.jsx
+++ src/global/App.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import Controls from '../components/Controls';
+import Gameboard from '../components/Gameboard';
+
 const App = () => {
+    console.log('render: App');
+    useEffect(() => {
+        console.log('mount: App');
+    }, []);
+
     return (
         <>
+            <Controls />
+            <Gameboard />
         </>
     )
 };
