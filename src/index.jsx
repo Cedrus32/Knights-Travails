fae21e84bef565dbfe8e9d51ab0e6e4a2e90d920
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-08-13 07:06:11 -0500

    Restore reset to full original -- remove unused rules after styling is complete

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './global/App';
 
+import './styles/reset.css';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
