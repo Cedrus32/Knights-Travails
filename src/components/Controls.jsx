8df80298b5ab46bd419dc1790edaca0eaecab3f9
Shannon <shannon@Shannons-MacBook-Pro.local>
2023-08-17 07:39:59 -0500

    Build up menubar and nested toolsets

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,22 +2,73 @@ import { useEffect } from 'react';
 import '../styles/controls.css';
 
 const controls = [
-    {name: 'place knight', id: 'start'},
-    {name: 'place end', id: 'end'},
-    {name: 'randomize'},
-    {name: 'step through'},
-    {name: 'play'},
-    {name: 'clear'},
+    {
+        group: 'choose placement',
+        elements: [
+            {name: 'place knight', id: 'start'},
+            {name: 'place end', id: 'end'},
+            {name: 'randomize'},
+        ]
+    },
+    {
+        group: 'show moves',
+        elements: [
+            {name: 'step through'},
+            {name: 'play'},
+        ]
+    },
+    {
+        group: 'clear',
+        elements: [
+            {name: 'clear'},
+        ]
+    }
 ];
 
 // NOTE: ? watch screen width, toggle small-width styling to <menu> as needed
-// <label htmlFor={control.id}>{control.name}</label>
-// <input type='text' id={control.id}></input>
-// <button type='button'>{control.name}</button>
 
-function generateSet(controlSet) {
-    console.log(controlSet);
+function generateSet(controlSet, index) {
+    let controls = [];
+    if (controlSet.group === 'choose placement' || controlSet.group === 'show moves') {
+        let tools = [];
+        let orientation;
+        controlSet.elements.forEach((item, i) => {
+            if (controlSet.group === 'place') {
+                orientation = 'vertical';
+            } else {
+                orientation = 'horizontal';
+            }
+            if (item.id) {
+                tools.push(
+                    <li key={'tool' + i}>
+                        <label htmlFor={item.id}>{item.name}</label>
+                        <input type='text' id={item.id}></input>
+                    </li>
+                )
+            } else {
+                tools.push (
+                    <li key={'tool' + i}>
+                        <button type='button'>{item.name}</button>
+                    </li>
+                )
+            }
+        });
+        controls.push(
+            <ul key={controlSet.group} role='toolset' aria-label={controlSet.group} aria-orientation={orientation}>
+                {tools}
+            </ul>
+        );
+    } else {
+        controls.push(
+            <button key={controlSet.group} type='button'>{controlSet.elements[0].name}</button>
+        )
+    }
 
+    return (
+        <li key={'menu' + index} role='menuitem'>
+            {controls}
+        </li>
+    )
 }
 
 const Controls = () => {
@@ -29,7 +80,7 @@ const Controls = () => {
     return (
         <menu role='menubar' aria-labelledby='Controls' aria-orientation='vertical'>
             <label>Controls</label>
-            {controls.map(set => generateSet(set))}
+            {controls.map((set, i) => generateSet(set, i))}
         </menu>
     )
 };
